35a1ec05bf1af676d27f1d616fadfb090dac1adb
Jack <jackm550@googlemail.com>
2017-09-25 16:03:50 +0100

    First Commit, added first controller & route + partials for layout file

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Homepage Route
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Test Route
+Route::get('test', 'TestController@index');
